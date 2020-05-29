c1d519e234c9d0c4e2c966423bb204a39490c3a4
Jaspal Singh <jsinghw94@gmail.com>
2020-05-29 18:53:59 -0400

    removed follow link for Anonymous users

--- tweet/models.py
+++ tweet/models.py
@@ -5,7 +5,7 @@ from twitter_user.models import Profile
 
 # Create your models here.
 class Tweet(models.Model):
-    content = models.TextField()
+    content = models.TextField(max_length=140)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
 
