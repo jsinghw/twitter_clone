8f4b075cb5ea8a469abe71e396343d438f4fd028
Jaspal Singh <jsinghw94@gmail.com>
2020-06-16 15:55:38 -0400

    class based views

--- tweet/views.py
+++ tweet/views.py
@@ -5,6 +5,8 @@ from tweet.models import Tweet
 from notification.models import Notification
 from twitter_user.models import Profile
 import re
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 @login_required
@@ -36,6 +38,39 @@ def add_tweet(request):
         html,
         {'form': form, 'notifcation_count': notifcation_count}
     )
+
+
+class AltAddTweet(LoginRequiredMixin, View):
+    def get(self, request):
+        html = 'add_tweet.html'
+        notifcation_count = Notification.objects.filter(user=request.user).count()
+        form = AddTweetForm()
+        return render(
+            request,
+            html,
+            {'form': form, 'notifcation_count': notifcation_count}
+        )
+
+    def post(self, request):
+        form = AddTweetForm(request.POST)
+        if form.is_valid():
+            new_tweet = form.save(commit=False)
+            new_tweet.profile = request.user
+            new_tweet.save()
+
+# check to see if there is any mentions and add notification if there is
+            reg = re.compile(r'(.*)(@\S*)')
+            m = reg.match(new_tweet.content)
+            if m:
+                if Profile.objects.filter(handle=m[2][1:]):
+                    new_notification = Notification(
+                        user=Profile.objects.get(handle=m[2][1:]),
+                        tweet=new_tweet
+                    )
+                    new_notification.save()
+
+        return HttpResponseRedirect(reverse('homepage'))
+        form = AddTweetForm()
 
 
 def detail_tweet(request, id):
